58eaca92bf11e90ad6d0545f6a47f30738524851
Hussam Aldeen <sam66ish@localhost.localdomain>
2020-08-02 15:58:32 +0200

    major changes to lists and dynamic list

--- lists.cpp
+++ lists.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
-#include "lists.hpp"
+#include "list_dyn.hpp"
+
 using namespace std;;
 
 //code mimicing the functonality of std::lists
 
 int main(){
-    list_dyn list = list_dyn();
+    list_dyn<int> list = list_dyn<int>();
     list.insert(1,0);
     list.insert(2,1);
     list.insert(3,2);
     list.insert(4,3);
 
-
+    try{
     cout << " the elements are " << list.elementAtInd(0) << " "
     << list.elementAtInd(1) << " " << list.elementAtInd(2) << " "
     << list.elementAtInd(3) << endl;
-
+    }
+    catch(exception& e){
+        cout << e.what() << endl;
+    }
     cout << "the size is " << list.size() << endl;
 
     list.deleteAtInd(1);
     cout << "the size after deleting the first element is " << list.size() << endl;
 
+    try{
     cout << "the elements are " << list.elementAtInd(0) << " "
     << list.elementAtInd(1) << " " << list.elementAtInd(2) << " "
     << list.elementAtInd(3) << endl;
+    }
+    catch(exception& e){
+        cout << e.what() << endl;
+    }
 }
--- lists.hpp
+++ lists.hpp
@@ -2,81 +2,19 @@
 
 using namespace std;;
 
-//custom structs similar to what std::vector might be
-typedef struct{
-    int *tab;
-    int capa;
-    int nb;
-}dynamic_table;
-
-
-
 //the type in which the list will accept
-typedef int TELEM; // type of element
+//typedef int TELEM; // type of element
 
-
+template <class T>
 class list{
 
     public:
     virtual int size() =0; //returns the size of the list
     virtual int elementAtInd(int i) =0; //returns the element at the index i
     virtual void deleteAtInd(int i) =0; // deletes the element at the index i
-    virtual void insert(TELEM e, int i) =0; // inserts the element e at the index i
+    virtual void insert(T e, int i) =0; // inserts the element e at the index i
+    virtual void reverse() ; //reverses the list.
 };
 
-class list_dyn:public list {
-    private:
-        dynamic_table ls;
-    public:
-        list_dyn() { // conlsuctor using dynamic table
-            ls.capa = 3; ls.nb = 0;
-            ls.tab = new TELEM[ls.capa];
-        }
 
-        ~list_dyn() { delete[] ls.tab;} // deconlsuctor
-        
-        int size() override{return ls.nb;}
-
-        TELEM elementAtInd(int i) override{
-
-            if(i >= ls.nb){
-                cout << "Error index " << i << " out of bounds" << endl;
-                return -999;
-            }else{
-                return ls.tab[i];
-            }
-            
-        }
-        
-        void deleteAtInd(int i) override{
-            
-            for(int j = i; j <= ls.nb-1; j++){
-                ls.tab[j] = ls.tab[j+1];
-            }
-            ls.nb -=1;
-        }
-
-        void insert(int e, int i) override{
-            
-            if (ls.nb == ls.capa){
-                int *temp;
-                temp = new int[ls.capa *2];
-                for(int i = 0; i <= ls.nb -1; i++){
-                    temp[i] = ls.tab[i];
-                 }
-                delete[] ls.tab;
-                ls.tab = temp;
-                ls.capa = ls.capa*2;
-               
-            }
-
-            for(int j = ls.nb-1; j >= i; j--){
-                ls.tab[j+1] = ls.tab[j];
-            }
-            ls.tab[i] = e;
-            ls.nb +=1;
-            
-        }
-
-};
 
